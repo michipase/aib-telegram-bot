7c91dff0f4707013457b43c351fbe2e9d20c2f09
mp97dev <michipase@gmail.com>
2024-01-08 16:53:25 +0100

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install python3 wkhtmltopdf -y
+sudo apt install python3 wkhtmltopdf libxml2-dev libxslt1-dev -y
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
